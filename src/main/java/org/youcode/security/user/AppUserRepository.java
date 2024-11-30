6843b4967af21871973a38db4f25705fbcce17b8
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-11-30 15:13:48 +0100

    repository  added

--- src/main/java/org/youcode/security/user/AppUserRepository.java
+++ src/main/java/org/youcode/security/user/AppUserRepository.java
@@ -1,4 +1,12 @@
 package org.youcode.security.user;
 
-public interface AppUserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
+
+    Optional<AppUser> findByEmail(String email);
+
 }
