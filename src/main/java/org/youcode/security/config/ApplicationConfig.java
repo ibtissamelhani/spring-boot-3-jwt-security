cc71ae93502c1203a29ac4613aa56478fcef9d7d
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-12-02 11:30:09 +0100

    ApplicationConfig class added

--- src/main/java/org/youcode/security/config/ApplicationConfig.java
+++ src/main/java/org/youcode/security/config/ApplicationConfig.java
@@ -3,8 +3,12 @@ package org.youcode.security.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.youcode.security.repository.AppUserRepository;
 
 @Configuration
@@ -17,5 +21,17 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService() {
         return username -> appUserRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("user not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
